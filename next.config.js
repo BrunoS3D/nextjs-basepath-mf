de0de037e4e7989e0b7fc914f74a57fe72303ccb
Bruno Silva <bruno3dcontato@gmail.com>
2022-07-07 17:49:11 -0300

    big bang

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/ui/ecom-ui-search',
+  assetPrefix: '/ui/ecom-ui-search/',
+}
+
+
+module.exports = nextConfig;
